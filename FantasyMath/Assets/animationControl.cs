559d560a5c2762fc03ffe6d3a197c1dc5c47fb60
gfrancoo <gianfranco.aguirre.cruz@gmail.com>
2020-09-17 22:37:25 -0500

    Animations

---
+++ FantasyMath/Assets/animationControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationControl : MonoBehaviour
+{
+    public GUIAnimFREE[] mainMenu;
+    public GUIAnimFREE[] playMenu;
+
+    private void Start()
+    {
+        foreach (GUIAnimFREE ui in mainMenu)
+        {
+            ui.PlayInAnims(GUIAnimSystemFREE.eGUIMove.Self);
+        }
+    }
+
+    public void openPlay()
+    {
+        foreach (GUIAnimFREE ui in playMenu)
+        {
+            ui.PlayInAnims(GUIAnimSystemFREE.eGUIMove.Self);
+        }
+    }
+}
